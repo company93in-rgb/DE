3d40e11b04b58cfdfa09ef31fbb3c614e7524fee
company93in-rgb <company.93.in@gmail.com>
2026-01-18 17:19:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9jtycRyMaTweY2zxlp115Fj_9rU0k9hU",
+    authDomain: "data-store-6137a.firebaseapp.com",
+    projectId: "data-store-6137a",
+    storageBucket: "data-store-6137a.firebasestorage.app",
+    messagingSenderId: "329665997160",
+    appId: "1:329665997160:web:09a57664fc7f0dd6a3cedd",
+    measurementId: "G-PBE7WGMDQG"
+  };
